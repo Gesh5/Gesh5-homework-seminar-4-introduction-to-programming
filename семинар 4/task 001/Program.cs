f2621830e0fbd59668c9f8f50cf55c35b2c60609
Gesh5 <129482161+Gesh5@users.noreply.github.com>
2023-04-25 18:35:44 +1000

    Add files via upload

---
+++ семинар 4/task 001/Program.cs
@@ -0,0 +1,19 @@
+﻿// принимает на вход два числа (А и В) и возводит 
+// число А в натуральную степень В
+
+// 3, 5 = 243
+// 2, 4 = 16
+
+
+Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+    step = step * a;
+}
+Console.WriteLine($"A в степени B равно: {step}");
+
